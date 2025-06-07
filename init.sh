aaf419c3e603444f3b4fdae8ee48b47738aa6999
Alan Mateus <76778046+alanmts-am@users.noreply.github.com>
2025-06-06 23:09:04 -0300

    fix: update tb_config_sistema after install .jar

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+java -jar pec.jar -console -url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -username=$POSTGRES_USER -password=$POSTGRES_PASSWORD -continue
 
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c "update tb_config_sistema set ds_texto = null, ds_inteiro = 1 where co_config_sistema = 'TREINAMENTO';"
-
-java -jar pec.jar -console -url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -username=$POSTGRES_USER -password=$POSTGRES_PASSWORD -continue
 
 unset PGPASSWORD
  
