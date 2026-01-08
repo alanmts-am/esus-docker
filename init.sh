5bab810e4f8ad641ddf536bc8d4bee5fe7cecc98
alanmts-am <alanmts.am@gmail.com>
2026-01-08 17:28:11 -0300

    fix: add database validation after Java command

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 java -jar pec.jar -console -url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" -username=$POSTGRES_USER -password=$POSTGRES_PASSWORD -continue
 
+until psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT 1 FROM tb_config_sistema LIMIT 1;" > /dev/null 2>&1; do
+  sleep 5
+done
+
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -c "update tb_config_sistema set ds_texto = null, ds_inteiro = 1 where co_config_sistema = 'TREINAMENTO';"
 
 unset PGPASSWORD
